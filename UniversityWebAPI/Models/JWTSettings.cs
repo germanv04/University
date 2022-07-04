1f71ec9135945d65cae2373e321babaacbdc1fde
GermanJesus04 <germanjesus04@gmail.com>
2022-07-03 23:33:09 -0500

    Configuracion del JWT

---
+++ UniversityWebAPI/Models/JWTSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace UniversityWebAPI.Models
+{
+    public class JWTSettings
+    {
+        public bool ValidateIssuerSigninKey { get; set; }
+        public string? IssuerSigninKey { get; set; }
+
+        public bool ValidateIssuer { get; set; }
+        public string? Issuer { get; set; }
+
+        public bool ValidateAudience { get; set; }
+        public string? validAudience { get; set; }
+
+        public bool RequireExperationTime { get; set; }
+        public bool ValidateLifeTime { get; set; }
+    }
+}
---
+++ UniversityWebAPI/Models/UserTokens.cs
@@ -0,0 +1,14 @@
+﻿namespace UniversityWebAPI.Models
+{
+    public class UserTokens
+    {
+        public Guid Id { get; set; }
+        public string Token { get; set; }
+        public string UserName { get; set; }
+        public TimeSpan Validity { get; set; }
+        public string RefreshToken { get; set; }
+        public string EmailId { get; set; }
+        public Guid Guid { get; set; }
+        public DateTime ExpiredTime { get; set; }
+    }
+}
